5991a1b3bcb15e0d16928d384a300e1e01c1b063
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-08-16 18:56:13 +0530

    Shared TextField Component

---
+++ client/src/shared/TextField/TextField.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { TextField as MuiTextField } from '@material-ui/core';
+
+export default function Input(props) {
+
+    const { name, label, value, onChange, placeholder } = props;
+
+    return (
+        <MuiTextField
+            fullWidth
+            placeholder={placeholder}
+            variant="outlined"
+            label={label}
+            name={name}
+            value={value}
+            onChange={onChange}
+        />
+    );
+};
