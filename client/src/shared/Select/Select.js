82bd81810db2f912a6af3ef5ca1a7a412d05ee7c
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-08-16 18:55:58 +0530

    Shared Select Component

---
+++ client/src/shared/Select/Select.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FormControl, InputLabel, MenuItem, Select as MuiSelect } from '@material-ui/core';
+
+export default function Select(props) {
+
+    const { error, helperText, label, options, onChange, value } = props;
+
+    return (
+        <FormControl
+            fullWidth
+            variant="outlined"
+            error={error}
+        >
+            <InputLabel>{label}</InputLabel>
+            <MuiSelect
+                renderValue={() => `${helperText}`}
+                label={label}
+                onChange={onChange}
+                value={value}
+
+            >
+                {
+                    options.map(
+                        (option) => (
+                            <MenuItem key={option.id} value={option.title}>{option.title}</MenuItem>
+                        ),
+                    )
+                }
+            </MuiSelect>
+        </FormControl>
+    );
+};
