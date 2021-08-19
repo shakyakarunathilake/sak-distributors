e53dd27a9e587536d5a0dcea6d0b73ebd0689654
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-08-19 23:51:03 +0530

    Minor Changes

--- client/src/shared/TextField/TextField.js
+++ client/src/shared/TextField/TextField.js
@@ -3,7 +3,7 @@ import { TextField as MuiTextField } from '@material-ui/core';
 
 export default function Input(props) {
 
-    const { error, helperText, name, label, value, onChange, placeholder } = props;
+    const { name, label, value, onChange, placeholder } = props;
 
     return (
         <MuiTextField
