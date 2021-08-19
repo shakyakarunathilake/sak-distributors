15b9d380c803a4bf0a896181fa7cfa96acbf4120
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-08-19 22:17:21 +0530

    Updated useForm

--- client/src/components/useForm.js
+++ client/src/components/useForm.js
@@ -1,30 +1,33 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 export function useForm(initialFieldValues) {
 
     const [values, setValues] = useState(initialFieldValues);
+    const [errors, setErrors] = useState({});
 
     const handleInputChange = e => {
-        const { name, value } = e.target
+        const { name, value } = e.target;
         setValues({
             ...values,
             [name]: value
-        })
+        });
         // if (validateOnChange)
         //     validate({ [name]: value })
-    }
+    };
 
     return (
+        errors,
         values,
+        setErrors,
         setValues,
         handleInputChange
     );
 };
 
-export function Form(props) {
-    return (
-        <form>
-            {props.children}
-        </form>
-    );
-};
+// export function Form(props) {
+//     return (
+//         <form>
+//             {props.children}
+//         </form>
+//     )
+// };
