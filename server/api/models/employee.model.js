2940bc90c7d3b8d9afba0e2913802a81069d714b
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-08-21 23:39:34 +0530

    employee route and model updated

--- server/api/models/employee.model.js
+++ server/api/models/employee.model.js
@@ -2,17 +2,18 @@ const mongoose = require("mongoose");
 
 const employeeSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    employeeid: { type: String, required: true },
-    employeeimage: { type: String, required: true },
+    employeeid: { type: String, required: false },
+    //employeeimage: { type: String, required: false },
     fullname: { type: String, required: true },
     callingname: { type: String, required: true },
+    email: { type: String, requied: true },
     dob: { type: Date, required: true },
-    age: { type: Number, required: true },
+    hireddate: { type: Date, required: true },
     address: { type: String, required: true },
     nic: { type: String, required: true },
     gender: { type: String, required: true },
-    mobilenumber: { type: Number, required: true },
-    telephonenumber: { type: Number, required: true },
+    phonenumber: { type: Number, required: true },
+    role: { type: String, required: true },
     designation: { type: String, required: true },
     civilstatus: { type: String, required: true },
     employeestatus: { type: String, required: true }
--- server/api/routes/employee.route.js
+++ server/api/routes/employee.route.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const { response } = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const multer = require("multer");  /* For images */
 
-const Employee = require("../models/employees.model");
+const Employee = require("../models/employee.model");
 
 const storage = multer.diskStorage({
     destination: function (req, file, callback) {
@@ -19,7 +18,7 @@ const uploads = multer({ storage: storage });
 
 router.get("/", (req, res, next) => {
     res.status(200).json({
-        message: "Handeling GET requests to /employees",
+        message: "Handling GET requests to /employees",
         info: "This is information about SAK Distributors' Employees"
     });
 });
@@ -29,16 +28,17 @@ router.post("/create-employee", uploads.single('photo'), (req, res, next) => {
     const employee = new Employee({
         _id: new mongoose.Types.ObjectId(),
         employeeid: req.body.employeeid,
-        employeeimage: `localhost:8080/${req.body.employeeid}.jpg`,
+        // employeeimage: `localhost:8080/${req.body.employeeid}.jpg`,
         fullname: req.body.fullname,
         callingname: req.body.callingname,
+        email: req.body.email,
         dob: req.body.dob,
-        age: req.body.age,
+        hireddate: req.body.hireddate,
         address: req.body.address,
         nic: req.body.nic,
         gender: req.body.gender,
-        mobilenumber: req.body.mobilenumber,
-        telephonenumber: req.body.telephonenumber,
+        phonenumber: req.body.phonenumber,
+        role: req.body.role,
         designation: req.body.designation,
         civilstatus: req.body.civilstatus,
         employeestatus: req.body.employeestatus
@@ -48,7 +48,7 @@ router.post("/create-employee", uploads.single('photo'), (req, res, next) => {
         .save()
         .then(result => {
             res.status(201).json({
-                message: "Handeling POST requests to /employees/create-employee",
+                message: "Handling POST requests to /employees/create-employee, EMPLOYEE SAVED",
                 addedEmployee: result
             });
         })
