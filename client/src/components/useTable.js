1c683026a9d700e8cd289b9117e3067826e9ec55
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-08-27 14:13:25 +0530

    useTable Component Update

--- client/src/components/useTable.js
+++ client/src/components/useTable.js
@@ -1,15 +1,58 @@
 import React from 'react';
-import { Table } from '@material-ui/core';
+import classnames from 'classnames';
 
-export default function useTable(records, headCells) {
+//Material UI Components
+import {
+    Table as MuiTable,
+    TableContainer as MuiTableContainer,
+    TableHead as MuiTableHead,
+    TableBody as MuiTableBody,
+    TableRow as MuiTableRow,
+    TableCell as MuiTableCell,
+    Paper,
+} from '@material-ui/core';
 
-    const TblContainer = props => (
-        <Table>
+//Material UI Icons
+import EditIcon from '@material-ui/icons/Edit';
+import VisibilityIcon from '@material-ui/icons/Visibility';
 
-        </Table>
+//SCSS Styles
+import style from './useTable.module.scss';
+
+export default function useTable(thead, tbody) {
+
+    const TableContainer = props => (
+        <MuiTableContainer className={style.tablecontainer} component={Paper}>
+            <MuiTable className={style.table}>
+                <MuiTableHead className={style.tablehead}>
+                    <MuiTableRow className={style.tableheadrow}>
+                        {thead.map((x, i) => (
+                            <MuiTableCell align="center" className={style.tablecell} key={i}>
+                                {x}
+                            </MuiTableCell>
+                        ))}
+                    </MuiTableRow>
+                </MuiTableHead>
+                <MuiTableBody>
+                    {tbody.map((x, i) => (
+                        <MuiTableRow className={classnames({ [style.greytablerow]: i % 2 === 1 })} key={i}>
+                            {x.map((y, i) => (
+                                <MuiTableCell align="right" key={i}>
+                                    {y}
+                                </MuiTableCell>
+                            ))}
+                            <MuiTableCell align="center" className={style.grid}>
+                                <VisibilityIcon className={style.visibilityIcon} />
+                                <EditIcon className={style.editIcon} />
+                            </MuiTableCell>
+                        </MuiTableRow>
+                    ))}
+                </MuiTableBody>
+            </MuiTable>
+        </MuiTableContainer>
     )
 
     return {
-        TblContainer
+        TableContainer
     }
 }
