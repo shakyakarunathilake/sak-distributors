68fd1055a0e203f9529e8033a3aa6f60e4be9f77
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-08-29 22:09:27 +0530

    TableContainer, TableHead, TablePagination Initial Push

--- client/src/components/useTable.js
+++ client/src/components/useTable.js
@@ -1,85 +1,68 @@
-import React from 'react';
-import classnames from 'classnames';
+import React, { useState } from 'react';
 
 //Material UI Components
 import {
-    Table as MuiTable,
+    TablePagination as MuiTablePagination,
     TableContainer as MuiTableContainer,
     TableHead as MuiTableHead,
-    TableBody as MuiTableBody,
-    TableRow as MuiTableRow,
-    TableCell as MuiTableCell,
     Paper,
 } from '@material-ui/core';
-
-//Material UI Icons
-import EditIcon from '@material-ui/icons/Edit';
-import VisibilityIcon from '@material-ui/icons/Visibility';
 
 //SCSS Styles
 import style from './useTable.module.scss';
 
-export default function useTable(thead, tbody) {
+export default function useTable(thead, records) {
+
+    const pages = [5, 10, 25, 50];
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(pages[page]);
+    const [order, setOrder] = useState();
+    const [orderBy, setOrderBy] = useState();
 
     const TableContainer = props => (
         <MuiTableContainer className={style.tablecontainer} component={Paper}>
-            <MuiTable className={style.table}>
-                <MuiTableHead className={style.tablehead}>
-                    <MuiTableRow className={style.tableheadrow}>
-                        {thead.map((x, i) => (
-                            <MuiTableCell
-                                align="center"
-                                className={classnames(
-                                    { [style.columnonesticky]: i === 0 },
-                                    { [style.columntwosticky]: i === 1 },
-                                    { [style.columnlaststicky]: i === thead.length - 1 },
-                                    style.tablecell
-                                )}
-                                key={i}
-                            >
-                                {x}
-                            </MuiTableCell>
-                        ))}
-                    </MuiTableRow>
-                </MuiTableHead>
-                <MuiTableBody className={style.tablebody}>
-                    {tbody.map((x, i) => (
-                        <MuiTableRow
-                            className={classnames(
-                                { [style.greytablerow]: i % 2 === 1 },
-                                { [style.whitetablerow]: i % 2 === 0 },
-                            )}
-                            key={i}
-                        >
-                            {x.map((y, i) => (
-                                <MuiTableCell
-                                    className={classnames(
-                                        { [style.columnonesticky]: i === 0 },
-                                        { [style.columntwosticky]: i === 1 },
-                                    )}
-                                    align={classnames(
-                                        { "right": i != 1 && i != 0 }
-                                    )}
-                                    key={i}
-                                >
-                                    {y}
-                                </MuiTableCell>
-                            ))}
-                            <MuiTableCell
-                                align="center"
-                                className={style.columnlaststicky}
-                            >
-                                <VisibilityIcon className={style.visibilityIcon} />
-                                <EditIcon className={style.editIcon} />
-                            </MuiTableCell>
-                        </MuiTableRow>
-                    ))}
-                </MuiTableBody>
-            </MuiTable>
+            {props.children}
         </MuiTableContainer>
     )
 
+    const TableHead = props => {
+        return (
+            <MuiTableHead className={style.tablehead}>
+                {props.children}
+            </MuiTableHead>
+        )
+    }
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    }
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+
+    }
+
+    const TablePagination = () => (
+        <MuiTablePagination
+            component="div"
+            page={page}
+            rowsPerPageOptions={pages}
+            rowsPerPage={rowsPerPage}
+            count={records.length}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+    )
+
+    const recordsAfterPagingAndSorting = () => {
+        return records.slice(page * rowsPerPage, (page + 1) * rowsPerPage);
+    }
+
     return {
-        TableContainer
+        TableContainer,
+        TableHead,
+        TablePagination,
+        recordsAfterPagingAndSorting,
     }
 }
