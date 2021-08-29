aee637807f28fae01873dd6f61d07ef7d2d89b34
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-08-29 22:09:58 +0530

    Customer Updated

--- client/src/pages/CustomersPage/Customers.js
+++ client/src/pages/CustomersPage/Customers.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 
 //Shared Components
 import useTable from '../../components/useTable';
 import Page from '../../shared/Page/Page';
+import TextField from '../../shared/TextField/TextField';
+
+//Material UI 
+import Button from '@material-ui/core/Button';
+import { Table, TableBody, TableRow, TableCell } from '@material-ui/core';
+import { InputAdornment } from '@material-ui/core';
+
+
+//Material UI Icons
+import EditIcon from '@material-ui/icons/Edit';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import SearchIcon from '@material-ui/icons/Search';
 
 //SCSS Styles
 import style from './Customers.module.scss';
@@ -12,26 +26,125 @@ import axios from 'axios';
 
 export default function Customers() {
     axios
-        .get("http://localhost:8080/customers/")
+        .get("http://localhost:8080/customers/get-all-customers")
         .then(res => {
-            console.log(res.data);
+            localStorage.setItem("AllCustomerData", JSON.stringify(res.data));
         })
         .catch(error => {
             console.log(error);
         })
 
-    const { TblContainer } = useTable();
+    //Getting Data From Local Storage
+    const AllCustomerData = JSON.parse(localStorage.getItem("AllCustomerData"));
+
+    //Deconstructing into thead and tbody
+    const { thead, tbody } = AllCustomerData;
+
+    //Setting the tbody as records in useState
+    const [records, setRecords] = useState(tbody);
+
+    //Passing and Importing data to useTable
+    const {
+        TableContainer,
+        TableHead,
+        TablePagination,
+        recordsAfterPagingAndSorting
+    } = useTable(thead, records);
 
     return (
         <Page
             title="Customers"
-            buttonText="Customer"
         >
-            <div className={style.container}>
-                <div className={style.tableRow}>
-                    <TblContainer>
-                    </TblContainer>
+            <div className={style.actionRow}>
+                <div className={style.search}>
+                    <TextField
+                        className={style.searchtextfield}
+                        placeholder="Search"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon />
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
                 </div>
+                <Button
+                    className={style.button}
+                    color="primary"
+                    size="medium"
+                    variant="contained">
+                    <AddCircleIcon className={style.icon} />
+                    Add New Customer
+                </Button>
+            </div>
+            <div className={style.tablecontainer}>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow className={style.tableheadrow}>
+                                {
+                                    thead.map((x, i) => (
+                                        <TableCell
+                                            align="center"
+                                            className={classnames(
+                                                { [style.columnonesticky]: i === 0 },
+                                                { [style.columntwosticky]: i === 1 },
+                                                style.tablecell
+                                            )}
+                                            key={x.id}
+                                        >
+                                            {x.label}
+                                        </TableCell>
+                                    ))
+                                }
+                                <TableCell
+                                    align="center"
+                                    className={classnames(style.columnlaststicky, style.tableheadfontstyles)}
+                                >
+                                    Action
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody className={style.tablebody}>
+                            {
+                                recordsAfterPagingAndSorting().map((x, i) => (
+                                    <TableRow
+                                        className={classnames(
+                                            { [style.greytablerow]: i % 2 === 1 },
+                                            { [style.whitetablerow]: i % 2 === 0 },
+                                        )}
+                                        key={i}
+                                    >
+                                        <TableCell className={style.columnonesticky}> {x.customerid} </TableCell>
+                                        <TableCell className={style.columntwosticky}> {x.storename} </TableCell>
+                                        <TableCell> {x.brn} </TableCell>
+                                        <TableCell> {x.fullname} </TableCell>
+                                        <TableCell> {x.title} </TableCell>
+                                        <TableCell> {x.firstname} </TableCell>
+                                        <TableCell> {x.lastname} </TableCell>
+                                        <TableCell> {x.route} </TableCell>
+                                        <TableCell> {x.billingaddress} </TableCell>
+                                        <TableCell> {x.shippingaddress} </TableCell>
+                                        <TableCell> {x.storecontactnumber} </TableCell>
+                                        <TableCell> {x.customercontactnumber} </TableCell>
+                                        <TableCell> {x.email} </TableCell>
+                                        <TableCell> {x.addeddate} </TableCell>
+                                        <TableCell
+                                            align="center"
+                                            className={style.columnlaststicky}
+                                        >
+                                            <VisibilityIcon className={style.visibilityIcon} />
+                                            <EditIcon className={style.editIcon} />
+                                        </TableCell>
+                                    </TableRow>
+
+                                ))
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination />
             </div>
         </Page>
     );
