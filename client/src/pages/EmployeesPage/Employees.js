4f760764b721329175629ed4eb81a3e24a62fc85
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-09-05 12:18:47 +0530

    Employees Updated

--- client/src/pages/EmployeesPage/Employees.js
+++ client/src/pages/EmployeesPage/Employees.js
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import classnames from 'classnames';
 
 //Shared Components
-import useTable from '../../components/useTable';
 import Page from '../../shared/Page/Page';
+import Popup from '../../shared/Popup/Popup';
 import TextField from '../../shared/TextField/TextField';
+import useTable from '../../components/useTable';
+// import useTableSearch from '../../components/useTableSearch';
 
 //Material UI 
 import Button from '@material-ui/core/Button';
-import { TableContainer, Paper, Table, TableHead, TableBody, TableRow, TableCell, TableSortLabel } from '@material-ui/core';
+import { TableContainer, Paper, Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
 import { InputAdornment } from '@material-ui/core';
-
 
 //Material UI Icons
 import EditIcon from '@material-ui/icons/Edit';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import SearchIcon from '@material-ui/icons/Search';
+
+//
+import EmployeesForm from './EmployeesForm';
 
 //SCSS Styles
 import style from './Employees.module.scss';
@@ -25,15 +29,18 @@ import style from './Employees.module.scss';
 import axios from 'axios';
 
 export default function Employees() {
-    axios
-        .get("http://localhost:8080/employees/get-all-employees")
-        .then(res => {
-            localStorage.setItem("AllEmployeeData", JSON.stringify(res.data));
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    ;
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:8080/employees/get-all-employees")
+            .then(res => {
+                localStorage.setItem("AllEmployeeData", JSON.stringify(res.data));
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        ;
+    })
 
     console.log(JSON.parse(localStorage.getItem("AllEmployeeData")));
 
@@ -49,114 +56,175 @@ export default function Employees() {
     //Passing and Importing data to useTable
     const {
         TablePagination,
-        TableHead,
-        // handleSortRequest,
-        recordsAfterPagingAndSorting
-    } = useTable(thead, records);
+        recordsAfterPaging,
+    } = useTable(records);
+
+    const [recordForEdit, setRecordForEdit] = useState(null)
+    const [openPopup, setOpenPopup] = useState(false);
+
+    const addOrEdit = (employee, resetForm) => {
+        if (employee.employeeid == 0) {
+            axios
+                .post("http://localhost:8080/employees/create-employee", {
+                    // employeeFormData})
+                    "employeeid": employee.employeeid,
+                    //employeeimage: '',
+                    "title": employee.title,
+                    "fullname": employee.fullname,
+                    "firstname": employee.firstname,
+                    "lastname": employee.lastname,
+                    "email": employee.email,
+                    "dob": employee.dob,
+                    "hireddate": employee.hireddate,
+                    "address": employee.address,
+                    "nic": employee.nic,
+                    "gender": employee.gender,
+                    "contactnumber": employee.contactnumber,
+                    "designation": employee.designation,
+                    "civilstatus": employee.civilstatus,
+                    "employeestatus": employee.employeestatus,
+                })
+                .then(res => {
+                    console.log(res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+        else {
+            axios
+                .put("http://localhost:8080/employees/update-by-id/:employeeId", employee)
+                .then(res => {
+                    console.log(res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+        resetForm()
+        setRecordForEdit(null)
+        setOpenPopup(false)
+        setRecords(AllEmployeeData)
+    }
+
 
     return (
         <Page
             title="Employees"
         >
-            <div className={style.actionRow}>
-                <div className={style.search}>
-                    <TextField
-                        className={style.searchtextfield}
-                        placeholder="Search"
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <SearchIcon />
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
+            <div className={style.container}>
+
+                <div className={style.actionRow}>
+                    <div className={style.search}>
+                        <TextField
+                            // onChange={e => setSearchVal(e.target.value)}
+                            className={style.searchtextfield}
+                            placeholder="Search"
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <SearchIcon />
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                    </div>
+                    <Button
+                        className={style.button}
+                        color="primary"
+                        size="medium"
+                        variant="contained"
+                        onClick={
+                            () => setOpenPopup(true)
+                        }
+                    >
+                        <AddCircleIcon className={style.icon} />
+                        Add New Employee
+                    </Button>
                 </div>
-                <Button
-                    className={style.button}
-                    color="primary"
-                    size="medium"
-                    variant="contained">
-                    <AddCircleIcon className={style.icon} />
-                    Add New Employee
-                </Button>
-            </div>
-            <div className={style.pagecontent}>
-                <TableContainer className={style.tablecontainer} component={Paper} >
-                    <Table>
-                        <TableHead />
-                        {/* <TableHead className={style.tablehead}>
-                            <TableRow className={style.tableheadrow}>
-                                {
-                                    thead.map((x, i) => (
-                                        <TableCell
-                                            align="center"
-                                            className={classnames(
-                                                { [style.columnonesticky]: i === 0 },
-                                                { [style.columntwosticky]: i === 1 },
-                                                style.tablecell
-                                            )}
-                                            key={x.id}
-                                        >
-                                            <TableSortLabel
-                                                direction={orderBy === x.id ? order : "asc"}
-                                                className={style.tablesortlabel}
-                                                onClick={() => { handleSortRequest(x.id) }}
+
+                <div className={style.pagecontent}>
+                    <TableContainer className={style.tablecontainer} component={Paper} >
+                        <Table>
+                            <TableHead className={style.tablehead}>
+                                <TableRow className={style.tableheadrow}>
+                                    {
+                                        thead.map((x, i) => (
+                                            <TableCell
+                                                align="left"
+                                                // className={classnames(
+                                                //     { [style.columnone]: i === 0 },
+                                                //     { [style.columntwo]: i === 1 },
+                                                //     { [style.columnthree]: i === 2 },
+                                                //     style.tablecell
+                                                // )}
+                                                className={style.tablecell}
+                                                key={x.id}
                                             >
                                                 {x.label}
-                                            </TableSortLabel>
-                                        </TableCell>
+                                            </TableCell>
+                                        ))
+                                    }
+                                    <TableCell
+                                        align="center"
+                                        className={classnames(style.actioncolumn, style.tableheadfontstyles)}
+                                    >
+                                        Action
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody className={style.tablebody}>
+                                {
+                                    recordsAfterPaging().map((row, i) => (
+                                        <TableRow
+                                            className={classnames(
+                                                { [style.greytablerow]: i % 2 === 1 },
+                                                { [style.whitetablerow]: i % 2 === 0 },
+                                            )}
+                                            key={i}
+                                        >
+                                            {
+                                                row.map((cell, i) => (
+                                                    <TableCell
+                                                        // className={classnames(
+                                                        //     { [style.columnone]: i === 0 },
+                                                        //     { [style.columntwo]: i === 1 },
+                                                        //     { [style.columnthree]: i === 2 },
+                                                        // )}
+                                                        key={cell.id}
+                                                    >
+                                                        {cell.label}
+                                                    </TableCell>
+                                                ))
+                                            }
+                                            <TableCell
+                                                align="center"
+                                                className={style.actioncolumn}
+                                            >
+                                                <VisibilityIcon className={style.visibilityIcon} />
+                                                <EditIcon className={style.editIcon} />
+                                            </TableCell>
+                                        </TableRow>
+
                                     ))
                                 }
-                                <TableCell
-                                    align="center"
-                                    className={classnames(style.columnlaststicky, style.tableheadfontstyles)}
-                                >
-                                    Action
-                                </TableCell>
-                            </TableRow>
-                        </TableHead> */}
-                        <TableBody className={style.tablebody}>
-                            {
-                                recordsAfterPagingAndSorting().map((x, i) => (
-                                    <TableRow
-                                        className={classnames(
-                                            { [style.greytablerow]: i % 2 === 1 },
-                                            { [style.whitetablerow]: i % 2 === 0 },
-                                        )}
-                                        key={i}
-                                    >
-                                        <TableCell className={style.columnonesticky}> {x.employeeid} </TableCell>
-                                        <TableCell className={style.columntwosticky}> {x.fullname} </TableCell>
-                                        <TableCell> {x.title} </TableCell>
-                                        {/* <TableCell> {x.firstname} </TableCell>
-                                        <TableCell> {x.lastname} </TableCell> */}
-                                        <TableCell> {x.employeestatus} </TableCell>
-                                        <TableCell> {x.designation} </TableCell>
-                                        <TableCell> {x.hireddate} </TableCell>
-                                        <TableCell> {x.nic} </TableCell>
-                                        <TableCell> {x.contactnumber} </TableCell>
-                                        <TableCell> {x.email} </TableCell>
-                                        <TableCell> {x.address} </TableCell>
-                                        <TableCell> {x.dob} </TableCell>
-                                        <TableCell> {x.gender} </TableCell>
-                                        <TableCell> {x.civilstatus} </TableCell>
-                                        <TableCell
-                                            align="center"
-                                            className={style.columnlaststicky}
-                                        >
-                                            <VisibilityIcon className={style.visibilityIcon} />
-                                            <EditIcon className={style.editIcon} />
-                                        </TableCell>
-                                    </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination />
+                </div >
 
-                                ))
-                            }
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination />
+                <Popup
+                    title='New Employee'
+                    openPopup={openPopup}
+                    setOpenPopup={setOpenPopup}
+                >
+                    <EmployeesForm
+                        recordForEdit={recordForEdit}
+                        addOrEdit={addOrEdit}
+                    />
+                </Popup>
             </div>
-        </Page>
+        </Page >
     );
 };
