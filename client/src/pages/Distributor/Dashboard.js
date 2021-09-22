aa78fccf9eb7550b2627e890e450767f5ffef3f1
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-09-22 22:15:51 +0530

    Dashboard Update

--- client/src/pages/Distributor/Dashboard.js
+++ client/src/pages/Distributor/Dashboard.js
@@ -3,20 +3,29 @@ import React from 'react';
 //Shared Components
 import Page from '../../shared/Page/Page';
 
-// import style from './Dashboard.module.scss';
+//SCSS style
+import style from './Dashboard.module.scss';
 
 export default function Dashboard() {
 
-    const firsttimelogin = JSON.parse(localStorage.getItem("Auth")).firsttimelogin;
+    const firsttimelogin = JSON.parse(sessionStorage.getItem("Auth")).firsttimelogin;
     if (firsttimelogin) {
         window.alert("Please change your password");
         window.location.replace("http://localhost:3000/change-password");
     }
 
-    return (
-        <Page
-            title="Dashboard">
+    const [value, setValue] = React.useState(new Date());
 
+    return (
+        <Page title="Dashboard">
+            <div className={style.container}>
+                <div className={style.paper}>
+
+                </div>
+                <div className={style.paper}>
+
+                </div>
+            </div>
         </Page>
     )
 };
