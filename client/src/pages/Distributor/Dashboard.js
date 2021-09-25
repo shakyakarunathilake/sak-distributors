860bbbc41248400c481b79740e51b6a9e1be7db7
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-09-25 16:33:10 +0530

    dashboard updated

--- client/src/pages/Distributor/Dashboard.js
+++ client/src/pages/Distributor/Dashboard.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import style from './Dashboard.module.scss';
 
 //Material UI Components
 import isWeekend from 'date-fns/isWeekend';
@@ -15,10 +16,19 @@ import Page from '../../shared/Page/Page';
 //images
 import user from '../../images/user.svg';
 
-//SCSS style
-import style from './Dashboard.module.scss';
-
 export default function Dashboard() {
+
+    const [value, setValue] = React.useState(new Date());
+    // const [orientation, setOrientation] = useState();
+
+    // function settingOrientation() {
+    //     const width = window.innerWidth;
+    //     if (width >= 1220) {
+    //         setOrientation("portrait")
+    //     } else {
+    //         setOrientation("landscape")
+    //     }
+    // }
 
     const firsttimelogin = JSON.parse(sessionStorage.getItem("Auth")).firsttimelogin;
     const employeeimage = JSON.parse(sessionStorage.getItem("Auth")).employeeimage;
@@ -33,16 +43,13 @@ export default function Dashboard() {
         window.location.replace("http://localhost:3000/change-password");
     }
 
-    const [value, setValue] = React.useState(new Date());
-
     return (
         <Page title="Dashboard">
-            <div className={style.container}>
-                <div className={style.columnDivider}>
+            <div className={style.mediumScreen}>
+
+                <div className={style.scrollable}>
+
                     <div className={style.userInfo}>
-                        <div className={style.welcomeMsg}>
-                            Welcome {firstname}
-                        </div>
                         <div className={style.imageRow}>
                             <img src={employeeimage.value ? employeeimage : user} alt="" />
                         </div>
@@ -50,7 +57,7 @@ export default function Dashboard() {
                             <div className={style.textDiv}>
                                 <div className={style.text}>
                                     <div className={style.boldtext}>
-                                        Employee ID:
+                                        Emp. ID:
                                     </div>
                                     <div className={style.regulartext}>
                                         {employeeid}
@@ -94,6 +101,23 @@ export default function Dashboard() {
                             </div>
                         </div>
                     </div>
+
+                    <div className={style.personalNotes}>
+
+                    </div>
+
+                    <div className={style.notifications}>
+
+                    </div>
+
+                    <div className={style.recentActivities}>
+
+                    </div>
+
+                </div>
+
+                <div className={style.sticky}>
+
                     <div className={style.calendar}>
                         <LocalizationProvider dateAdapter={AdapterDateFns}>
                             <StaticDatePicker
@@ -108,8 +132,220 @@ export default function Dashboard() {
                             />
                         </LocalizationProvider>
                     </div>
+
+                    <div className={style.onlineUsers}>
+
+                    </div>
+
                 </div>
+
             </div>
+
+            <div className={style.largeScreen}>
+
+                <div className={style.scrollable}>
+
+                    <div className={style.userInfo}>
+                        <div className={style.infoRow}>
+                            <div className={style.imgCol}>
+                                <img src={employeeimage.value ? employeeimage : user} alt="" />
+                            </div>
+                            <div className={style.infoCol}>
+                                <div className={style.textDiv}>
+                                    <div className={style.text}>
+                                        <div className={style.boldtext}>
+                                            Emp. ID:
+                                        </div>
+                                        <div className={style.regulartext}>
+                                            {employeeid}
+                                        </div>
+                                    </div>
+                                    <div className={style.text}>
+                                        <div className={style.boldtext}>
+                                            Name:
+                                        </div>
+                                        <div className={style.regulartext}>
+                                            {firstname} {lastname}
+                                        </div>
+                                    </div>
+                                    <div className={style.text}>
+                                        <div className={style.boldtext}>
+                                            Email:
+                                        </div>
+                                        <div className={style.regulartext}>
+                                            {email}
+                                        </div>
+                                    </div>
+                                    <div className={style.text}>
+                                        <div className={style.boldtext}>
+                                            Role:
+                                        </div>
+                                        <div className={style.regulartext}>
+                                            {role}
+                                        </div>
+                                    </div>
+                                    <div className={style.text}>
+                                        <div className={style.boldtext}>
+                                            Status:
+                                        </div>
+                                        <div className={style.regulartext}>
+                                            {employeestatus}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={style.btnRow}>
+                            <Button variant="contained"> Change Password</Button>
+                        </div>
+                    </div>
+
+                    <div className={style.personalNotes}>
+
+                    </div>
+
+                    <div className={style.notifications}>
+
+                    </div>
+
+                    <div className={style.recentActivities}>
+
+                    </div>
+
+                </div>
+
+                <div className={style.sticky}>
+
+                    <div className={style.calendar}>
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <StaticDatePicker
+                                orientation="portrait"
+                                openTo="day"
+                                value={value}
+                                shouldDisableDate={isWeekend}
+                                onChange={(newValue) => {
+                                    setValue(newValue);
+                                }}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        </LocalizationProvider>
+                    </div>
+
+                    <div className={style.onlineUsers}>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className={style.extraLargeScreen}>
+
+                <div className={style.scrollable}>
+
+                    <div className={style.columnA}>
+
+                    <div className={style.userInfo}>
+                        <div className={style.infoRow}>
+                            <div className={style.imgCol}>
+                                <img src={employeeimage.value ? employeeimage : user} alt="" />
+                            </div>
+                            <div className={style.infoCol}>
+                                <div className={style.textDiv}>
+                                    <div className={style.text}>
+                                        <div className={style.boldtext}>
+                                            Emp. ID:
+                                        </div>
+                                        <div className={style.regulartext}>
+                                            {employeeid}
+                                        </div>
+                                    </div>
+                                    <div className={style.text}>
+                                        <div className={style.boldtext}>
+                                            Name:
+                                        </div>
+                                        <div className={style.regulartext}>
+                                            {firstname} {lastname}
+                                        </div>
+                                    </div>
+                                    <div className={style.text}>
+                                        <div className={style.boldtext}>
+                                            Email:
+                                        </div>
+                                        <div className={style.regulartext}>
+                                            {email}
+                                        </div>
+                                    </div>
+                                    <div className={style.text}>
+                                        <div className={style.boldtext}>
+                                            Role:
+                                        </div>
+                                        <div className={style.regulartext}>
+                                            {role}
+                                        </div>
+                                    </div>
+                                    <div className={style.text}>
+                                        <div className={style.boldtext}>
+                                            Status:
+                                        </div>
+                                        <div className={style.regulartext}>
+                                            {employeestatus}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={style.btnRow}>
+                            <Button variant="contained"> Change Password</Button>
+                        </div>
+                    </div>
+
+                        <div className={style.personalNotes}>
+
+                        </div>
+
+                    </div>
+
+                    <div className={style.columnB}>
+
+                        <div className={style.notifications}>
+
+                        </div>
+
+                        <div className={style.recentActivities}>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className={style.sticky}>
+
+                    <div className={style.calendar}>
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <StaticDatePicker
+                                orientation="landscape"
+                                openTo="day"
+                                value={value}
+                                shouldDisableDate={isWeekend}
+                                onChange={(newValue) => {
+                                    setValue(newValue);
+                                }}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        </LocalizationProvider>
+                    </div>
+
+                    <div className={style.onlineUsers}>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
         </Page>
     )
 };
