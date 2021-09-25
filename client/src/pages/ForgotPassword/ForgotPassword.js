f3a9f641eaa8177beb9f30f882528c53f8b9e578
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-09-25 16:32:50 +0530

    react hook form

--- client/src/pages/ForgotPassword/ForgotPassword.js
+++ client/src/pages/ForgotPassword/ForgotPassword.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './ForgotPassword.module.scss';
 
 //Shared Components
-import useForm from '../../components/useForm';
+import { useForm, Controller } from 'react-hook-form';
 import TextField from '../../shared/TextField/TextField';
 
 //Logo
@@ -11,83 +11,149 @@ import logo from '../../images/logo.svg';
 //Material UI Exports
 import Button from '@material-ui/core/Button';
 import { InputAdornment } from '@material-ui/core';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 //Material UI Icons
 import EmailIcon from '@material-ui/icons/Email';
 
 //Connecting to Backend
-// import axios from 'axios';
+import axios from 'axios';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export default function Login() {
 
-    const postData = () => {
-        window.location.replace("http://localhost:3000/dashboard");
-    }
+    const { handleSubmit, formState: { errors }, control, reset } = useForm({
+        defaultValues: {
+            email: "",
+        }
+    });
 
-    const initialFieldValues = {
-        email: '',
-    }
+    const [open, setOpen] = React.useState(false);
+    const [alert, setAlert] = React.useState();
+    const [type, setType] = React.useState();
 
-    const {
-        values,
-        handleInputChange,
-    } = useForm({ initialFieldValues })
+    const handleAlert = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+
+
+    const onSubmit = (values) => {
+        axios
+            .post("http://localhost:8080/password/forgot-password/", {
+                "email": values.email,
+            })
+            .then(res => {
+                sessionStorage.setItem("Response", JSON.stringify(res.data));
+                const type = JSON.parse(sessionStorage.getItem("Response")).type;
+                const message = JSON.parse(sessionStorage.getItem("Response")).message;
+                setAlert(message);
+                setType(type);
+                handleAlert();
+            })
+            .catch(error => {
+                console.log(error)
+            })
+            ;
+        reset({
+            email: "",
+        });
+    };
 
     return (
         <div className={style.container}>
 
-            <div className={style.login}>
-                <div className={style.top}>
-                    <div className={style.logo}>
-                        <img src={logo} alt="" />
-                    </div>
-                    <div className={style.heading}>
-                        <span>Forgot Password ?</span>
-                    </div>
-                    <div className={style.description}>
-                        <p>
-                            Enter the email address you used when you joined
-                            and we’ll send you instructions to reset your password. 
-                            
-                            <br />
-                            <br />
-                            
-                            For security reasons, we do NOT store your password. 
-                            So rest assured that we will never send your password via email. 
-                        </p>
-                    </div>
-                    <div className={style.textfield}>
-                        <TextField
-                            className={style.field}
-                            label="Email"
-                            name="email"
-                            onChange={handleInputChange}
-                            value={values.email}
-                            InputProps={{
-                                endAdornment: (
-                                    <InputAdornment position="end">
-                                        <EmailIcon />
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </div>
-                    <div className={style.div}>
-                        <div>
-                            <Button
-                                className={style.button}
-                                color="primary"
-                                onClick={postData}
-                                variant="contained">
-                                Send Reset Instruction
-                            </Button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <div className={style.login}>
+                    <div className={style.top}>
+                        <div className={style.logo}>
+                            <img src={logo} alt="" />
+                        </div>
+                        <div className={style.heading}>
+                            <span>Forgot Password?</span>
+                        </div>
+                        <div className={style.description}>
+                            <p>
+                                Enter the email address you used when you joined
+                                and we’ll send you instructions to reset your password.
+
+                                <br />
+                                <br />
+
+                                For security reasons, we do NOT store your password.
+                                So rest assured that we will never send your password via email.
+                            </p>
+                        </div>
+                        <div className={style.textfield}>
+                            <Controller
+                                name={"email"}
+                                control={control}
+                                rules={{ required: true }}
+                                render={({ field: { onChange, value } }) => (
+                                    <TextField
+                                        className={style.field}
+                                        helperText="Email is required"
+                                        error={errors.email ? true : false}
+                                        onChange={onChange}
+                                        value={value}
+                                        label="Email"
+                                        type="text"
+                                        InputProps={{
+                                            endAdornment: (
+                                                <InputAdornment position="end">
+                                                    <EmailIcon />
+                                                </InputAdornment>
+                                            ),
+                                        }}
+                                    />
+                                )}
+                            />
+                        </div>
+                        <div className={style.div}>
+                            <div>
+                                <Button
+                                    className={style.button}
+                                    color="primary"
+                                    type="submit"
+                                    variant="contained">
+                                    Send Reset Instruction
+                                </Button>
+                            </div>
                         </div>
                     </div>
+                    <div className={style.copyright}>
+                        © 2021 SAK Distributors.All rights reserved.
+                    </div>
                 </div>
-                <div className={style.copyright}>
-                    © 2021 SAK Distributors. All rights reserved.
-                </div>
-            </div>
+            </form>
+            <Snackbar
+                open={open}
+                autoHideDuration={2500}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+            >
+                <Alert
+                    onClose={handleClose}
+                    severity={type}
+                    sx={{ width: '100%' }}
+                >
+                    {alert}
+                </Alert>
+            </Snackbar>
         </div >
     )
 }
