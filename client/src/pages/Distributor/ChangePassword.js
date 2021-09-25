6b2b30c7e34c0875a33b27b3afa56e3a056759dc
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-09-25 16:32:12 +0530

    react hook form

--- client/src/pages/Distributor/ChangePassword.js
+++ client/src/pages/Distributor/ChangePassword.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import style from './ChangePassword.module.scss';
 
 //Shared Components
 import Page from '../../shared/Page/Page';
-import useForm from '../../components/useForm';
+import { useForm, Controller } from 'react-hook-form';
 import TextField from '../../shared/TextField/TextField';
 
 //Logo
@@ -19,26 +20,44 @@ import MuiAlert from '@mui/material/Alert';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
-//SCSS style
-import style from './ChangePassword.module.scss';
-
 //Connecting to Backend
 import axios from 'axios';
-
 
 const Alert = React.forwardRef(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
-
 
 export default function ChangePassword() {
 
     const employeeid = JSON.parse(sessionStorage.getItem("Auth")).employeeid;
     const firsttimelogin = JSON.parse(sessionStorage.getItem("Auth")).firsttimelogin;
 
+    const { handleSubmit, formState: { errors }, control, reset } = useForm({
+        defaultValues: {
+            currentpassword: '',
+            newpassword: '',
+            confirmpassword: '',
+        }
+    });
+
+    const [currentPasswordShown, setCurrentPasswordShown] = useState(false);
+    const [newPasswordShown, setNewPasswordShown] = useState(false);
+    const [confirmPasswordShown, setConfirmPasswordShown] = useState(false);
     const [open, setOpen] = React.useState(false);
     const [alert, setAlert] = React.useState();
     const [type, setType] = React.useState();
+
+    const toggleCurrentPasswordVisiblity = () => {
+        setCurrentPasswordShown(currentPasswordShown ? false : true);
+    };
+
+    const toggleNewPasswordVisiblity = () => {
+        setNewPasswordShown(newPasswordShown ? false : true);
+    }
+
+    const toggleConfirmPasswordVisiblity = () => {
+        setConfirmPasswordShown(confirmPasswordShown ? false : true);
+    }
 
     const handleAlert = () => {
         setOpen(true);
@@ -51,7 +70,6 @@ export default function ChangePassword() {
         setOpen(false);
     };
 
-
     const handleFirstTimeLogin = () => {
         if (firsttimelogin) {
             setAlert("Warning: Please change your password");
@@ -60,10 +78,7 @@ export default function ChangePassword() {
         }
     };
 
-
-    const handleSubmit = e => {
-        e.preventDefault();
-
+    const onSubmit = (values) => {
         if (values.newpassword !== values.confirmpassword) {
             setType(true);
             setAlert("New Password and Confirm Password doesn't match");
@@ -87,126 +102,138 @@ export default function ChangePassword() {
                     console.log(err);
                 });
             ;
-            resetForm();
+            reset({
+                currentpassword: '',
+                newpassword: '',
+                confirmpassword: '',
+            });
         }
     }
-
-    const initialFieldValues = {
-        currentpassword: '',
-        newpassword: '',
-        confirmpassword: '',
-        showPassword: false
-    }
-
-    const {
-        values,
-        resetForm,
-        handleInputChange,
-        handleClickShowPassword,
-        handleMouseDownPassword
-    } = useForm({ initialFieldValues })
 
     return (
         <Page title="Change Password" className={style.page}>
             <div className={style.container} onLoad={handleFirstTimeLogin}>
 
-                <div className={style.blurDiv}>
-                    <div className={style.paper}>
-                        <div className={style.logo}>
-                            <img src={logo} alt="" />
-                        </div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className={style.blurDiv}>
+                        <div className={style.paper}>
+                            <div className={style.logo}>
+                                <img src={logo} alt="" />
+                            </div>
 
-                        <div className={style.heading}>
-                            <span>Emp. ID: {employeeid}</span>
-                        </div>
+                            <div className={style.heading}>
+                                <span>Emp. ID: {employeeid}</span>
+                            </div>
 
-                        <div className={style.textfield}>
-                            <TextField
-                                className={style.field}
-                                label="Current Password"
-                                onChange={handleInputChange}
-                                value={values?.currentpassword || ''}
-                                name="currentpassword"
-                                type={values.showPassword ? 'text' : 'password'}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                color="inherit"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                                edge="end"
-                                            >
-                                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                        </div>
+                            <div className={style.textfield}>
+                                <Controller
+                                    name={"currentpassword"}
+                                    control={control}
+                                    rules={{ required: true }}
+                                    render={({ field: { onChange, value } }) => (
+                                        <TextField
+                                            className={style.field}
+                                            helperText="Current Password is required"
+                                            error={errors.currentpassword ? true : false}
+                                            onChange={onChange}
+                                            value={value}
+                                            label="Current Password"
+                                            type={currentPasswordShown ? "text" : "password"}
+                                            InputProps={{
+                                                endAdornment: (
+                                                    <InputAdornment position="end">
+                                                        <IconButton
+                                                            color="inherit"
+                                                            onClick={toggleCurrentPasswordVisiblity}
+                                                            edge="end"
+                                                        >
+                                                            {currentPasswordShown ? <Visibility /> : <VisibilityOff />}
+                                                        </IconButton>
+                                                    </InputAdornment>
+                                                ),
+                                            }}
+                                        />
+                                    )}
+                                />
+                            </div>
 
-                        <div className={style.textfield}>
-                            <TextField
-                                className={style.field}
-                                label="New Password"
-                                name="newpassword"
-                                onChange={handleInputChange}
-                                type={values.showPassword ? 'text' : 'password'}
-                                value={values?.newpassword || ''}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                color="inherit"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                                edge="end"
-                                            >
-                                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                        </div>
+                            <div className={style.textfield}>
+                                <Controller
+                                    name={"newpassword"}
+                                    control={control}
+                                    rules={{ required: true }}
+                                    render={({ field: { onChange, value } }) => (
+                                        <TextField
+                                            className={style.field}
+                                            helperText="New Password is required"
+                                            error={errors.newpassword ? true : false}
+                                            onChange={onChange}
+                                            value={value}
+                                            label="New Password"
+                                            type={newPasswordShown ? "text" : "password"}
+                                            InputProps={{
+                                                endAdornment: (
+                                                    <InputAdornment position="end">
+                                                        <IconButton
+                                                            color="inherit"
+                                                            onClick={toggleNewPasswordVisiblity}
+                                                            edge="end"
+                                                        >
+                                                            {newPasswordShown ? <Visibility /> : <VisibilityOff />}
+                                                        </IconButton>
+                                                    </InputAdornment>
+                                                ),
+                                            }}
+                                        />
+                                    )}
+                                />
+                            </div>
 
-                        <div className={style.textfield}>
-                            <TextField
-                                className={style.field}
-                                label="Confirm Password"
-                                name="confirmpassword"
-                                onChange={handleInputChange}
-                                type={values.showPassword ? 'text' : 'password'}
-                                value={values?.confirmpassword || ''}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                color="inherit"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                                edge="end"
-                                            >
-                                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                        </div>
+                            <div className={style.textfield}>
+                                <Controller
+                                    name={"confirmpassword"}
+                                    control={control}
+                                    rules={{ required: true }}
+                                    render={({ field: { onChange, value } }) => (
+                                        <TextField
+                                            className={style.field}
+                                            helperText="Confirm Password is required"
+                                            error={errors.confirmpassword ? true : false}
+                                            onChange={onChange}
+                                            value={value}
+                                            label="Confirm Password"
+                                            type={confirmPasswordShown ? "text" : "password"}
+                                            InputProps={{
+                                                endAdornment: (
+                                                    <InputAdornment position="end">
+                                                        <IconButton
+                                                            color="inherit"
+                                                            onClick={toggleConfirmPasswordVisiblity}
+                                                            edge="end"
+                                                        >
+                                                            {confirmPasswordShown ? <Visibility /> : <VisibilityOff />}
+                                                        </IconButton>
+                                                    </InputAdornment>
+                                                ),
+                                            }}
+                                        />
+                                    )}
+                                />
+                            </div>
 
-                        <div className={style.div}>
-                            <Button
-                                className={style.button}
-                                color="primary"
-                                onClick={handleSubmit}
-                                variant="contained">
-                                Submit
-                            </Button>
+                            <div className={style.div}>
+                                <Button
+                                    className={style.button}
+                                    color="primary"
+                                    type="submit"
+                                    variant="contained"
+                                >
+                                    Submit
+                                </Button>
+                            </div>
                         </div>
                     </div>
-                </div>
-
+                </form>
                 <Snackbar
                     open={open}
                     autoHideDuration={2500}
