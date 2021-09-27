03a6f56a77b997328fc5b7d93ebe1dfff4c7e9a8
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-09-27 22:07:31 +0530

    SnackBarAlert shared component initial push

---
+++ client/src/shared/SnackBarAlert/SnackBarAlert.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+//Material UI Components
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function SnackBarAlert(props) {
+
+    const { type, message } = props;
+    const [open, setOpen] = React.useState(false);
+
+    const handleAlert = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    return (
+        <Snackbar open={open} autoHideDuration={2500} onClose={handleClose} >
+            <Alert onClose={handleClose} severity={type} sx={{ width: '100%' }}>
+                {message}
+            </Alert>
+        </Snackbar>
+    )
+}
