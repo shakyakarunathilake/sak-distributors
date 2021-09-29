45b997f5e9ea114c6fac398dfa8cb8419e5ae5d8
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-09-29 12:08:12 +0530

    Moved the table body to manageEmployee

--- client/src/components/useTable.js
+++ client/src/components/useTable.js
@@ -3,91 +3,54 @@ import classnames from 'classnames';
 
 //Material UI 
 import {
-    TableContainer,
+    TableContainer as MuiTableContainer,
     Paper,
     Table as MuiTable,
-    TableHead,
-    TableBody,
+    TableHead as MuiTableHead,
     TableRow,
     TableCell
 } from '@material-ui/core';
 
-//Material UI Icons
-import EditIcon from '@material-ui/icons/Edit';
-import VisibilityIcon from '@material-ui/icons/Visibility';
-
 //SCSS styles
 import style from './useTable.module.scss';
 
-export default function useTable(headCells, records) {
+export default function useTable(headCells) {
 
-    const Table = (props) => (
-        <TableContainer className={style.tablecontainer} component={Paper}>
+    const TableContainer = (props) => (
+        <MuiTableContainer className={style.tablecontainer} component={Paper}>
             <MuiTable>
-                <TableHead className={style.tablehead}>
-                    <TableRow className={style.tableheadrow}>
-                        {
-                            headCells.map((x, i) => (
-                                <TableCell
-                                    align="left"
-                                    className={style.tablecell}
-                                    key={i}
-                                >
-                                    {x}
-                                </TableCell>
-                            ))
-                        }
-                        <TableCell
-                            align="center"
-                            className={classnames(style.actioncolumn, style.tableheadfontstyles)}
-                        >
-                            Action
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody className={style.tablebody}>
-                    {
-                        records.map((row, i) => (
-                            <TableRow
-                                className={classnames(
-                                    { [style.greytablerow]: i % 2 === 1 },
-                                    { [style.whitetablerow]: i % 2 === 0 },
-                                )}
-                                key={i}
-                            >
-                                {
-                                    row.map((cell, i) => (
-                                        <TableCell key={i} >
-                                            {cell}
-                                        </TableCell>
-                                    ))
-                                }
-                                <TableCell
-                                    align="center"
-                                    className={style.actioncolumn}
-                                >
-                                    <VisibilityIcon
-                                        className={style.visibilityIcon}
-                                    />
-                                    <EditIcon
-                                        className={style.editIcon}
-                                    // onClick={() => {
-                                    //     openInPopup(row[0].label);
-                                    // }}
-                                    />
-                                </TableCell>
-                            </TableRow>
-
-                        ))
-                    }
-                </TableBody>
+                {props.children}
             </MuiTable>
-        </TableContainer>
+        </MuiTableContainer>
     )
 
+    const TableHead = (props) => (
+        <MuiTableHead className={style.tablehead}>
+            <TableRow className={style.tableheadrow}>
+                {
+                    headCells.map((x, i) => (
+                        <TableCell
+                            align="left"
+                            className={style.tablecell}
+                            key={i}
+                        >
+                            {x}
+                        </TableCell>
+                    ))
+                }
+                <TableCell
+                    align="center"
+                    className={classnames(style.actioncolumn, style.tableheadfontstyles)}
+                >
+                    Action
+                </TableCell>
+            </TableRow>
+        </MuiTableHead>
+    )
 
     return {
-        Table,
+        TableContainer,
+        TableHead
     }
 }
 
