8f0f5e82bc501fa1f01255486f306cd038286d33
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-10-13 11:58:20 +0530

    updates to model

--- server/api/models/product.model.js
+++ server/api/models/product.model.js
@@ -5,11 +5,21 @@ const productSchema = mongoose.Schema({
     productid: { type: String, required: true },
     name: { type: String, required: true },
     supplier: { type: String, required: true },
-    productimage: { type: Object, required: true },
-    mrp: { type: String, required: true },
-    price: { type: String, required: true },
-    mfgdt: { type: String, required: true },
-    exp: { type: String, required: true }
+    productimage: { type: Object, required: false },
+    addeddate: { type: String, required: true },
+    addedby: { type: String, required: true },
+    variants: [{
+        variantid: { type: String, required: true },
+        type: { type: String, required: true },
+        bulkprice: { type: Number, required: true },
+        mrp: { type: String, required: true },
+        price: { type: String, required: true },
+        grnid: { type: String, required: true },
+        offercaption: { type: String, required: false },
+        status: { type: String, required: true },
+        addeddate: { type: String, required: true },
+        addedby: { type: String, required: true },
+    }]
 })
 
 module.exports = mongoose.model('Product', productSchema);
