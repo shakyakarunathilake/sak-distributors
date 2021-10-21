4ccbd9c9613fe85c39fe72f0fb1f41cb5a3191cd
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-10-21 22:27:07 +0530

    too many changes to route

--- server/api/routes/product.route.js
+++ server/api/routes/product.route.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const multer = require("multer");
-const { rawListeners } = require("../models/product.model");
 
 const Product = require("../models/product.model");
 
@@ -25,55 +24,15 @@ router.get("/", (req, res, next) => {
     });
 });
 
-
-//Get next product id
-router.get("/get-next-product-variant-grn-id", (req, res, next) => {
-
-    function pad(num, size) {
-        while (num.length < size) num = "0" + num;
-        return "P" + num;
-    }
-
-    Product
-        .find({}, { productid: 1, _id: 0 })
-        .exec()
-        .then(doc => {
-            const productidarray = doc.map(x => {
-                return parseInt(x.productid.slice(1))
-            })
-
-            const nextproductid = pad(String(Math.max(...productidarray) + 1), 6);
-            const nextvariantid = "PV0001";
-            const nextgrnid = "GRN0001";
-
-            console.log("PRODUCT ID: ", nextproductid);
-            console.log("VARIANTID: ", nextvariantid);
-            console.log("GRN ID:", nextgrnid);
-
-            // res.status(200).json({
-            //     message: "Handeling GET requests to /get-next-productid",
-            //     nextproductid: nextproductid,
-            //     nextvariantid: nextvariantid,
-            //     nextgrnid: nextgrnid
-            // });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ "Error": err });
-        })
-
-})
-
-
 //Get all table product data
-router.get("/get-all-products-table-data", (req, res, next) => {
+router.get("/get-all-product-table-data", (req, res, next) => {
 
     Product
         .find()
         .exec()
         .then(doc => {
-
             const thead = [
+                "",
                 "Prod. ID",
                 "Name",
                 "Supplier",
@@ -84,16 +43,18 @@ router.get("/get-all-products-table-data", (req, res, next) => {
                 "Type"
             ]
 
-            const tbody = doc.map(x => [
-                x.productid,
-                x.name,
-                x.supplier,
-                x.variants[0].variantid,
-                x.variants[0].price,
-                x.variants[0].mrp,
-                x.variants[0].status,
-                x.variants[0].type
-            ])
+            const tbody = doc.map(x => ({
+                "productid": x.productid,
+                "name": x.name,
+                "supplier": x.supplier,
+                "status": x.status,
+                "variants": x.variants.map(y => ({
+                    "variantid": y.variantid,
+                    "type": x.type,
+                    "price": x.price,
+                    "mrp": x.mrp,
+                }))
+            }))
 
             res.status(201).json({
                 message: "Handeling GET requests to /get-all-product-table-data",
@@ -108,14 +69,49 @@ router.get("/get-all-products-table-data", (req, res, next) => {
 })
 
 
+// Get next product id
+router.get("/get-next-productid", (req, res, next) => {
+
+    function pad(num, size) {
+        while (num.length < size) num = "0" + num;
+        return "P" + num;
+    }
+
+    Product
+        .find({}, { productid: 1, _id: 0 })
+        .exec()
+        .then(doc => {
+
+            let nextproductid;
+            
+            if (doc.length === 0) {
+                nextproductid = "P000001"
+            } else {
+                const productidarray = doc.map(x => {
+                    return parseInt(x.productid.slice(1))
+                })
+                nextproductid = pad(String(Math.max(...productidarray) + 1), 6);
+            }
+
+            res.status(200).json({
+                message: "Handeling GET requests to /get-next-productid",
+                nextproductid: nextproductid,
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ "Error": err });
+        })
+
+})
+
 //Create a product
 router.post("/create-product", uploads.single("productimage"), (req, res, next) => {
 
     console.log("Body: ", req.body);
-    console.log("Added Date: ", req.body.addeddate, "Variant Added Date:", req.body.variantaddeddate);
+    console.log("Added Date: ", req.body.addeddate);
 
     const addeddate = new Date(req.body.addeddate).toISOString().split('T')[0];
-    const variantaddeddate = new Date(req.body.variantaddeddate).toISOString().split('T')[0];
 
     const product = new Product({
         _id: mongoose.Types.ObjectId(),
@@ -123,20 +119,9 @@ router.post("/create-product", uploads.single("productimage"), (req, res, next) 
         name: req.body.name,
         supplier: req.body.supplier,
         productimage: `localhost:8080/${req.body.productid}.jpg`,
+        status: req.body.status,
         addeddate: addeddate,
         addedby: req.body.addedby,
-        variants: {
-            variantid: req.body.variantid,
-            type: req.body.type,
-            bulkprice: req.body.bulkprice,
-            mrp: req.body.mrp,
-            price: req.body.price,
-            grnid: req.body.grnid,
-            offercaption: req.body.offercaption,
-            status: req.body.status,
-            addeddate: variantaddeddate,
-            addedby: req.body.variantaddedby
-        },
 
     })
 
@@ -149,7 +134,7 @@ router.post("/create-product", uploads.single("productimage"), (req, res, next) 
             res.status(201).json({
                 message: "Handeling POST requests to /products/create-product, PRODUCT SAVED",
                 type: 'success',
-                alert: `${result.name} (${result.productid}) : ${result.variants[0].variantid} : ${result.variants[0].grnid} added`,
+                alert: `${result.name} (${result.productid}) added`,
                 addedProduct: result,
             })
         })
@@ -162,24 +147,58 @@ router.post("/create-product", uploads.single("productimage"), (req, res, next) 
         })
 });
 
+//Create new product variant
+router.post("/add-new-variant/:productid", uploads.single("productimage"), (req, res, next) => {
+
+    console.log("FormData: ", req.body);
+
+    Product
+        .findOneAndUpdate(
+            { "productid": req.params.productid },
+            {
+                $push: {
+                    'variants': {
+                        'variantid': req.body.variantid,
+                        'type': req.body.type,
+                        'bulkprice': req.body.bulkprice,
+                        'mrp': req.body.mrp,
+                        'sellingprice': req.body.sellingprice,
+                        'purchaseprice': req.body.purchaseprice,
+                        'offercaption': req.body.offercaption,
+                        'status': req.body.status,
+                        'addeddate': req.body.variantaddeddate,
+                        'addedby': req.body.variantaddedby
+                    }
+                }
+            },
+            { upsert: true }
+        )
+        .exec()
+        .then(doc => {
+            res.status(200).json({
+                message: "Handling POST requests to /products/add-new-variant/:productid , VARIANT SAVED",
+                type: 'success',
+                alert: `${doc.name} (${doc.productid}): ${req.body.variantid} added`,
+                addedVariant: doc
+            });
+        })
+        .catch(err => {
+            res.status(200).json({
+                type: 'error',
+                alert: `Something went wrong. Could not update product`,
+            });
+            console.log(err);
+        });
+});
 
 //Get product data by Product ID
-router.get("/:productid/:variantid", (req, res, next) => {
+router.get("/:productid", (req, res, next) => {
     const productid = req.params.productid;
-    const variantid = req.params.variantid;
 
     Product
         .findOne({ "productid": productid })
         .exec()
         .then(doc => {
-
-            let result = doc.variants.map(x => {
-                if (x.variantid = variantid) {
-                    return x
-                }
-            })
-
-            console.log(result)
 
             const product = {
                 "productid": doc.productid,
@@ -188,22 +207,13 @@ router.get("/:productid/:variantid", (req, res, next) => {
                 "productimage": doc.productimage,
                 "addeddate": doc.addeddate,
                 "addedby": doc.addedby,
-                "variantid": result[0].variantid,
-                "type": result[0].type,
-                "bulkprice": result[0].bulkprice,
-                "mrp": result[0].mrp,
-                "price": result[0].price,
-                "grnid": result[0].grnid,
-                "offercaption": result[0].offercaption,
-                "status": result[0].status,
-                "variantaddeddate": result[0].addeddate,
-                "variantaddedby": result[0].addedby,
+                "status": doc.status,
             };
 
             console.log(product);
 
             res.status(200).json({
-                message: "Handeling GET requests to /:productid/:variantid",
+                message: "Handeling GET requests to /:productid",
                 product: product
             });
         })
@@ -212,4 +222,50 @@ router.get("/:productid/:variantid", (req, res, next) => {
             res.status(500).json({ "Error ": err });
         })
 })
+
+//Update product data by Product ID
+router.post("/update-by-id/:productid/:variantid", uploads.single("productimage"), (req, res, next) => {
+
+    Product
+        .findOneAndUpdate(
+            { "productid": req.params.productid, "variants.variantid": req.params.variantid },
+            {
+                '$set': {
+                    'name': req.body.name,
+                    'productid': req.body.productid,
+                    'name': req.body.name,
+                    'supplier': req.body.supplier,
+                    'productimage': req.body.productimage,
+                    'addeddate': req.body.addeddate,
+                    'addedby': req.body.addedby,
+                    'variants.$.type': req.body.type,
+                    'variants.$.bulkprice': req.body.bulkprice,
+                    'variants.$.mrp': req.body.mrp,
+                    'variants.$.price': req.body.price,
+                    'variants.$.offercaption': req.body.offercaption,
+                    'variants.$.status': req.body.status,
+                    'variants.$.variantaddeddate': req.body.variantaddeddate,
+                    'variants.$.variantaddedby': req.body.variantaddedby
+                }
+            },
+            { upsert: true }
+        )
+        .exec()
+        .then(doc => {
+            res.status(200).json({
+                message: "Handling POST requests to /products/update-by-id/:productid, PRODUCT UPDATED",
+                type: 'success',
+                alert: `${doc.name} (${doc.productid}) : ${req.params.variantid} Updated`,
+                updatedProduct: doc
+            });
+        })
+        .catch(err => {
+            res.status(200).json({
+                type: 'error',
+                alert: `Something went wrong. Could not update product`,
+            });
+            console.log(err);
+        });
+});
+
 module.exports = router;
