671340066751e6c53345e1802259d9e5de31abc2
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-10-31 11:22:01 +0530

    AdminForm initial push

--- client/src/pages/HR/AdminForm.js
+++ client/src/pages/HR/AdminForm.js
@@ -1,9 +1,52 @@
 import React from 'react';
 
-export default function AdminForm() {
+import { useForm, Controller } from 'react-hook-form';
+import formData from 'form-data';
+
+//Material UI Icons
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+
+//SCSS Styles
+import style from './AdminForm.module.scss';
+
+export default function AdminForm(props) {
+
+    const { setOpenPopup, employeeRecords } = props;
+
+    const { handleSubmit, formState: { errors }, control, reset, setValue, getValues, clearErrors } = useForm();
+
+    const onSubmit = () => {
+
+    }
+    
     return (
-        <div>
-            
+        <div className={style.container}>
+
+            <div className={style.header}>
+                <div>{employeeRecords ? "Edit Employee" : "Add New Employee"}</div>
+                <div>
+                    <HighlightOffIcon
+                        className={style.icon}
+                        onClick={() => { setOpenPopup(false) }}
+                    />
+                </div>
+            </div>
+
+            <div className={style.body}>
+                <form
+                    className={style.form}
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <div className={style.minicolumn}>
+                        <div>
+
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
