ab7e00d3eb86d5a8fea53bc260d6e1606044040f
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-11-07 16:06:07 +0530

    added some material style overrides and commented them out

--- client/src/shared/PopUp/PopUp.js
+++ client/src/shared/PopUp/PopUp.js
@@ -1,22 +1,41 @@
 import React from 'react';
 
+import Draggable from 'react-draggable';
+
 //Material UI Components
 import { Dialog, DialogContent } from '@material-ui/core';
+import DialogTitle from '@mui/material/DialogTitle';
+import Paper from '@mui/material/Paper';
 
 //Material UI Styling
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
+    // root: {
+    //     ".MuiDialogContent-root": {
+    //         padding: "0px 24px 8px 24px !important"
+    //     },
+    //     "& .MuiDialog-paperWidthLg": {
+    //         maxWidth: "none !important"
+    //     },
+    //     "& .MuiDialog-paper": {
+    //         margin: 0
+    //     },
+    //     "& .MuiDialogTitle-root": {
+    //         padding: "4px !important"
+    //     }
+    // },
     content: {
         "&:first-child": {
             paddingTop: "12px"
         }
-    }
+    },
 });
+
 
 export default function PopUp(props) {
 
-    const { children, openPopup, setOpenPopup } = props;
+    const { children, openPopup, setOpenPopup, fullScreen, disableBackdropClick, hideBackdrop, disableEnforceFocus } = props;
     const classes = useStyles();
 
     const handleClose = () => {
@@ -24,7 +43,11 @@ export default function PopUp(props) {
     };
 
     return (
-        <Dialog open={openPopup} onClose={handleClose} maxWidth="lg">
+        <Dialog
+            onClose={handleClose}
+            open={openPopup}
+            maxWidth="lg"
+        >
             <DialogContent className={classes.content}>
                 {children}
             </DialogContent>
