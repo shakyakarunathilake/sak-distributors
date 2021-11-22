51a743333a2fab4406cfce818b62a4f4a522df3d
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2021-11-22 19:08:01 +0530

    added more fields to schema

--- server/api/models/purchaseorder.model.js
+++ server/api/models/purchaseorder.model.js
@@ -4,8 +4,15 @@ const purchaseOrderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     ponumber: { type: String, required: true },
     supplier: { type: String, required: true },
-    requesteditems: { type: Array, require: true },
-    createdat: { type: String, required: true }
+    status: { type: String, required: true },
+    createdat: { type: String, required: true },
+    createdby: { type: String, requried: true },
+    approvedby: { type: String, requried: false },
+    requesteditems: { type: Array, required: true },
+    grosstotal: { type: String, required: true },
+    receiveddiscounts: { type: String, required: true },
+    damagedexpireditems: { type: String, required: true },
+    total: { type: String, required: true },
 })
 
 module.exports = mongoose.model('PurchaseOrder', purchaseOrderSchema);
