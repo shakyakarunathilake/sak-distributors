fd845073f50fe81278b99b3f679ad6fe338104f8
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-01-02 16:08:23 +0530

    store route initial push

---
+++ server/api/routes/store.route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Store = require("../models/store.model");
+
+module.exports = router;
