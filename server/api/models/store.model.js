bbf9b2588064349cb3cf70ecad3f62e74f68abd3
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-01-02 19:47:31 +0530

    store scheme basic

--- server/api/models/store.model.js
+++ server/api/models/store.model.js
@@ -4,16 +4,12 @@ const storeSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     productid: { type: String, required: true },
     name: { type: String, required: true },
+    racknumber: { type: String, required: true },
     storequantity: [{
-        grnginnumber: { type: String, required: false },
-        date: { type: String, required: false },
-        piecespercase: { type: String, required: false },
         salesqtycases: { type: String, required: false },
         salesqtypieces: { type: String, required: false },
         freeqtycases: { type: String, required: false },
         freeqtypieces: { type: String, required: false },
-        damaged: { type: String, required: false },
-        returned: { type: String, required: false }
     }],
 
 })
