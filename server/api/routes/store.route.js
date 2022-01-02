6c758d98cb83a732367f8a04e3ce37c8d6cd27b3
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-01-02 19:51:11 +0530

    initial push of store.route

--- server/api/routes/store.route.js
+++ server/api/routes/store.route.js
@@ -4,4 +4,68 @@ const mongoose = require("mongoose");
 
 const Store = require("../models/store.model");
 
+
+//Check whether the endpoint works
+router.get("/", (req, res, next) => {
+    res.status(200).json({
+        message: "Handeling GET requests to /store"
+    });
+});
+
+//Get all table store data
+router.get("/get-all-store-table-data", (req, res, next) => {
+
+    Store
+        .find()
+        .exec()
+        .then(doc => {
+
+            let tbody = []
+            let rowid = 1;
+
+            doc.forEach(product => {
+                tbody.push({
+                    id: rowid,
+                    name: product.name,
+                    productid: product.productid,
+                });
+
+                let parentid = rowid;
+                rowid++;
+
+                product.storequantity.forEach(details => {
+                    tbody.push({
+                        id: rowid,
+                        parentid: parentid,
+                        productid: product.productid,
+                        name: product.name,
+                        grnginnumber: details.grnginnumber,
+                        date: details.date,
+                        piecespercase: details.piecespercase,
+                        salesqtycases: details.salesqtycases,
+                        salesqtypieces: details.salesqtypieces,
+                        freeqtycases: details.freeqtycases,
+                        freeqtypieces: details.freeqtypieces,
+                        damaged: details.damaged,
+                        returned: details.returned
+                    })
+
+                    rowid++;
+                })
+            });
+
+            res.status(201).json({
+                message: "Handeling GET requests to /get-all-store-table-data",
+                tbody: tbody,
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ "Error": err });
+        })
+})
+
+
+
+
 module.exports = router;
