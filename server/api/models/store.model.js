a927e32d2206527ac85427830bd4d3c71e676907
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-01-03 11:24:22 +0530

    updated store schema

--- server/api/models/store.model.js
+++ server/api/models/store.model.js
@@ -11,7 +11,28 @@ const storeSchema = mongoose.Schema({
         freeqtycases: { type: String, required: false },
         freeqtypieces: { type: String, required: false },
     }],
-
+    grn: [{
+        ponumber: { type: String, required: false },
+        grnnumber: { type: String, required: false },
+        date: { type: String, required: false },
+        piecespercase: { type: String, required: false },
+        price: { type: String, required: false },
+        salesqtycases: { type: String, required: false },
+        salesqtypieces: { type: String, required: false },
+        freeqtycases: { type: String, required: false },
+        freeqtypieces: { type: String, required: false },
+    }],
+    gin: [{
+        orderno: { type: String, required: false },
+        ginnumber: { type: String, required: false },
+        date: { type: String, required: false },
+        piecespercase: { type: String, required: false },
+        price: { type: String, required: false },
+        salesqtycases: { type: String, required: false },
+        salesqtypieces: { type: String, required: false },
+        freeqtycases: { type: String, required: false },
+        freeqtypieces: { type: String, required: false },
+    }]
 })
 
 module.exports = mongoose.model('Store', storeSchema);
