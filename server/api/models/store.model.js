2c6ca94597e29860796302547893e6ab9d5153aa
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-01-03 20:49:47 +0530

    store schema updated

--- server/api/models/store.model.js
+++ server/api/models/store.model.js
@@ -4,16 +4,15 @@ const storeSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     productid: { type: String, required: true },
     name: { type: String, required: true },
-    racknumber: { type: String, required: true },
+    racknumber: { type: String, required: false },
     storequantity: [{
-        salesqtycases: { type: String, required: false },
-        salesqtypieces: { type: String, required: false },
-        freeqtycases: { type: String, required: false },
-        freeqtypieces: { type: String, required: false },
+        salesqtycases: { type: Number, required: false },
+        salesqtypieces: { type: Number, required: false },
+        freeqtycases: { type: Number, required: false },
+        freeqtypieces: { type: Number, required: false },
     }],
-    grn: [{
-        ponumber: { type: String, required: false },
-        grnnumber: { type: String, required: false },
+    grngin: [{
+        grnnumberginnumber: { type: String, required: false },
         date: { type: String, required: false },
         piecespercase: { type: String, required: false },
         price: { type: String, required: false },
@@ -22,17 +21,6 @@ const storeSchema = mongoose.Schema({
         freeqtycases: { type: String, required: false },
         freeqtypieces: { type: String, required: false },
     }],
-    gin: [{
-        orderno: { type: String, required: false },
-        ginnumber: { type: String, required: false },
-        date: { type: String, required: false },
-        piecespercase: { type: String, required: false },
-        price: { type: String, required: false },
-        salesqtycases: { type: String, required: false },
-        salesqtypieces: { type: String, required: false },
-        freeqtycases: { type: String, required: false },
-        freeqtypieces: { type: String, required: false },
-    }]
 })
 
 module.exports = mongoose.model('Store', storeSchema);
