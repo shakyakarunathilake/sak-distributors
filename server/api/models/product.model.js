9e6e0114bc6958a640f7716812bfae310e51bb3b
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-01-08 22:27:38 +0530

    data type from number to decimal123

--- server/api/models/product.model.js
+++ server/api/models/product.model.js
@@ -1,3 +1,4 @@
+const { Decimal128 } = require("mongodb");
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
@@ -12,11 +13,11 @@ const productSchema = mongoose.Schema({
     variants: [{
         variantid: { type: String, required: true },
         type: { type: String, required: true },
-        piecespercase: { type: String, required: true },
-        bulkprice: { type: Number, required: true },
-        mrp: { type: Number, required: true },
-        purchaseprice: { type: Number, required: true },
-        sellingprice: { type: Number, required: true },
+        piecespercase: { type: Number, required: true },
+        bulkprice: { type: Decimal128 , required: true },
+        mrp: { type: Decimal128, required: true },
+        purchaseprice: { type: Decimal128, required: true },
+        sellingprice: { type: Decimal128, required: true },
         offercaption: { type: String, required: false },
         status: { type: String, required: true },
         addeddate: { type: String, required: true },
