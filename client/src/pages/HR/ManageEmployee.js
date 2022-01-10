8f809086b3e06e4b4712b8ec5e1c3c799d7003f5
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-01-10 20:59:30 +0530

    removed unwanted commented out import

--- client/src/pages/HR/ManageEmployee.js
+++ client/src/pages/HR/ManageEmployee.js
@@ -24,7 +24,6 @@ import ViewEmployee from './ViewEmployee';
 
 //Connecting to Backend
 import axios from 'axios';
-// import ApproveSubmit from './ApproveSubmit';
 
 const Alert = React.forwardRef(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
