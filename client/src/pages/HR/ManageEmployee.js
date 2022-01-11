7e98be4b98d83311cac0e8b7b06c820430841c50
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-01-11 19:57:08 +0530

    formatted the code

--- client/src/pages/HR/ManageEmployee.js
+++ client/src/pages/HR/ManageEmployee.js
@@ -275,6 +275,7 @@ export default function ManageEmployee() {
                                             }
                                         ]}
                                     />
+
                                 </div>
                             );
                         }}
