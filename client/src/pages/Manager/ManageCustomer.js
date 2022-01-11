c0ba81bc77690830da0846a1d178e8e1767c9a65
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-01-11 13:14:30 +0530

    formatted

--- client/src/pages/Manager/ManageCustomer.js
+++ client/src/pages/Manager/ManageCustomer.js
@@ -258,6 +258,7 @@ export default function ManageCustomer() {
                                             }
                                         ]}
                                     />
+                                    
                                 </div>
                             );
                         }}
