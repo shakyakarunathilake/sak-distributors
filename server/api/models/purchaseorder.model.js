8083102ae6995b2e9fa96b73846895a6d2d59926
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-03-03 21:51:30 +0530

    removed damagedmissingitems from purchase order model

--- server/api/models/purchaseorder.model.js
+++ server/api/models/purchaseorder.model.js
@@ -17,7 +17,6 @@ const purchaseOrderSchema = mongoose.Schema({
     items: { type: Array, required: true },
     grosstotal: { type: String, required: true },
     receiveddiscounts: { type: String, required: true },
-    damagedmissingitems: { type: String, required: true },
     total: { type: String, required: true },
 })
 
