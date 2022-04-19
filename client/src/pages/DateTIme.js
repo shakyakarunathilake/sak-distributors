819c899c96f79e4aaaf31d4f4d8ce59855a66604
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-04-19 21:59:35 +0530

    Needed in added

--- client/src/pages/DateTIme.js
+++ client/src/pages/DateTIme.js
@@ -1,49 +1,63 @@
 /**
-    Format
-    2021-01-07 07:48:01
+    Format : 2021-01-07 07:48:01
+    Needed in : 
+        Manager                     Confirm Order Status From (Complete Order Form)
+        Purchase Manager            Create Purchase Order Form 
+        Purchase Manger             Supplier Payment Form
+        Storekeeper                 Create GIN Form
+        Storekeeper                 Create GRN Form
+        Delivery Representative     Confirm Order Status Form (Delivery Form)
 **/
 export const ManualDateTime = '';
 
 
 /**
-    Format
-    2021-01-07 
+    Format : 2021-01-07 
+    Needed in : 
+        Human Resources             Employee Form
+        Manager                     Supplier Form
+        Manager                     Vehicle Form
+        Purchase Manager            Product Form
+        Purchase Manager            Product Variant Form
+        Purchase Manager            Quotations Form
+        Sales Representative        Customer Form
 **/
-export const ManualDate = '';
+export const ManualDate = '2021-10-01';
 
 
 /**
-    Format
-    202101071508 
-    Seconds ain't considered
+    Format : 202101071508 (Seconds ain't considered)
+    Needed in : 
+        Purchase Manager            Create Purchase Order Form 
 **/
 export const ManualPurchaseOrderDateTime = '';
 
 
 /**
-    Format
-    2021-01-07T09:23:24
+    Format : 2021-01-07T09:23:24
+    Needed in : 
+        Sales Representative          Create Order Form (Before you click the "CREATE NEW ORDER" button)
 **/
 export const ManualOrderDate = '2021-01-07T09:23:24';
 
 /**
-    Format
-    20210110
-    YYYYMMDD
+    Format : 20210110 (YYYYMMDD)
+    Needed in : 
+        Sales Representative          Create Order Form (Before you click the "CREATE NEW ORDER" button)
 **/
 export const ManualOrderIDDate = '20210116';
 
 
 /**
-    Format
-    2021-01-10
-    (If today is 2021-01-07, then ManualOrderDeliveryDate is 3 days ahead a.k.a 2021-01-10)
+    Format : 2021-01-10 (If today is 2021-01-07, then ManualOrderDeliveryDate is 3 days ahead a.k.a 2021-01-10)
+    Needed in : 
+        Sales Representative          Create Order Form (Before you click the "CREATE NEW ORDER" button)
 **/
 export const ManualOrderDeliveryDate = '2021-01-10';
 
 /**
-    Format
-    20210107171747
-    Seconds are considered
+    Format : 20210107171747 (Seconds are considered)
+    Needed in: 
+        Storekeeper                 Create GIN Form
 **/
 export const ManualGINDateTime = '20210116081236';
