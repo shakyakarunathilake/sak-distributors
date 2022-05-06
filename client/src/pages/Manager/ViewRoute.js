93b3b97f9d9d83c6c67fee541272cebb30c9c6a1
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-05-06 19:00:44 +0530

    view route

---
+++ client/src/pages/Manager/ViewRoute.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+//Steps
+import StepTwo from './RouteFormStepTwo';
+
+export default function ViewRoute(props) {
+
+    const { handleClosePopUp, setAction, routeRecords, action } = props;
+
+    const { handleSubmit, control } = useForm({
+        mode: "all",
+        defaultValues: {
+            routeid: routeRecords.routeid,
+            from: routeRecords.from,
+            to: routeRecords.to,
+            title: routeRecords.title,
+            status: routeRecords.status,
+            addedby: routeRecords.addedby,
+            addeddate: routeRecords.addeddate
+        }
+    });
+
+    const onSubmit = () => {
+        handleClosePopUp(false);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+            <StepTwo
+                control={control}
+                onSubmit={onSubmit}
+                action={action}
+                handleClosePopUp={handleClosePopUp}
+            />
+
+        </form >
+    )
+}
