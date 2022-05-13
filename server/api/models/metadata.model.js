05062a76d0fb24ad88f07d624964e9148c564984
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-05-13 23:54:25 +0530

    new meta data model

--- server/api/models/metadata.model.js
+++ server/api/models/metadata.model.js
@@ -2,9 +2,30 @@ const mongoose = require("mongoose");
 
 const metaDataSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    noofpurchaseordertobeapproved: { type: Array, required: false },
-    noofawaitinggrn: { type: Array, required: false },
-    noofcustomerorders: { type: Array, required: false },
+    noofpurchaseordertobeapproved: [{
+        ponumber: { type: String, required: false },
+        createdat: { type: String, required: false },
+        createdby: { type: String, required: false },
+    }],
+    noofawaitinggrn: [{
+        ponumber: { type: String, required: false },
+        grnnumber: { type: String, required: false },
+        status: { type: String, required: false },
+    }],
+    noofcustomerorders: [{
+        orderno: { type: String, required: false },
+        route: { type: String, required: false },
+        status: { type: String, required: false },
+    }],
+    promotions: [{
+        productid: { type: String, required: false },
+        name: { type: String, required: false },
+        productimage: { type: String, required: false },
+        variantid: { type: String, required: false },
+        variantstatus: { type: String, required: false },
+        offercaption: { type: String, required: false },
+        productstatus: { type: String, required: false },
+    }],
 })
 
 module.exports = mongoose.model('MetaData', metaDataSchema);
