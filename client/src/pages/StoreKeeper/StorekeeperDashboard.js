00f1c4865255364b92e75d6e14619c7016f661a2
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-05-13 18:42:44 +0530

    store keeper dashboard updated

--- client/src/pages/StoreKeeper/StorekeeperDashboard.js
+++ client/src/pages/StoreKeeper/StorekeeperDashboard.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 //Shared Components
 import PageTwo from '../../shared/PageTwo/PageTwo';
 import Profile from '../../shared/PageTwo/Profile';
+import CustomerOrders from '../DashboardPaper/CustomerOrders';
+import AwaitingGRN from '../DashboardPaper/AwaitingGRN';
 
 //SCSS Styles
 import style from './StorekeeperDashboard.module.scss';
+
+//Connecting to backend
+import axios from 'axios';
 
 export default function StorekeeperDashboard() {
 
     if (JSON.parse(sessionStorage.getItem("Auth")).firsttimelogin) {
         window.location.replace("http://localhost:3000/change-password");
     }
+
+    let endpoints = [
+        "http://localhost:8080/dashboard/awaiting-grn",
+        "http://localhost:8080/dashboard/no-of-customer-orders",
+    ]
+
+    const [customerOrders, setCustomerOrders] = useState([]);
+    const [grn, setGrn] = useState([]);
+
+    useEffect(() => {
+        axios
+            .all(endpoints.map((endpoint) => axios.get(endpoint, { headers: { 'authorization': JSON.parse(sessionStorage.getItem("Auth")).accessToken } })))
+            .then(
+                axios.spread((...responses) => {
+                    setGrn(responses[0].data.noofawaitinggrn)
+                    setCustomerOrders(responses[1].data.noofcustomerorders)
+                })
+            )
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
 
     return (
         <PageTwo title="Dashboard">
@@ -22,6 +49,14 @@ export default function StorekeeperDashboard() {
                 </div>
 
                 <div className={style.columnB}>
+
+                    <AwaitingGRN
+                        grn={grn}
+                    />
+
+                    <CustomerOrders
+                        customerOrders={customerOrders}
+                    />
 
                 </div>
 
