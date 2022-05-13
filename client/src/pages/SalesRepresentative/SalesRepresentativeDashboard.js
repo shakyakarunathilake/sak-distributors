f4d5ad71f4be0d75dc59a2c247aa1862c9b86f0e
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-05-13 18:41:56 +0530

    promotions added to sales representative dashboard

--- client/src/pages/SalesRepresentative/SalesRepresentativeDashboard.js
+++ client/src/pages/SalesRepresentative/SalesRepresentativeDashboard.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 //Shared Components
 import TabletPageTwo from '../../shared/PageTwo/TabletPageTwo';
 import TabletProfile from '../../shared/PageTwo/TabletProfile';
+import Promotions from '../DashboardPaper/Promotions';
 
 //SCSS Styles
 import style from './SalesRepresentativeDashboard.module.scss';
+
+//Connecting to Backend
+import axios from 'axios';
 
 export default function SalesRepresentativeDashboard() {
 
     if (JSON.parse(sessionStorage.getItem("Auth")).firsttimelogin) {
         window.location.replace("http://localhost:3000/change-password");
     }
-    
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:8080/dashboard/promotions`, {
+                headers: {
+                    'authorization': JSON.parse(sessionStorage.getItem("Auth")).accessToken
+                }
+            })
+            .then(res => {
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
+
     return (
         <TabletPageTwo>
             <div className={style.container}>
@@ -22,8 +40,7 @@ export default function SalesRepresentativeDashboard() {
                 </div>
 
                 <div className={style.rowB}>
-
-
+                    <Promotions />
                 </div>
             </div>
         </TabletPageTwo>
