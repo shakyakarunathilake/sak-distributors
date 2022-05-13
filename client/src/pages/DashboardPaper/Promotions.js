4b36041b1a585d2d0e186019e1bb368265ec23a6
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-05-13 18:41:07 +0530

    promotions initial push

---
+++ client/src/pages/DashboardPaper/Promotions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//SCSS Style 
+import style from './Promotions.module.scss';
+
+export default function Promotions() {
+
+    return (
+        <div className={style.container}>
+
+        </div>
+    )
+}
