489270c7a90b9eb39f308efcef855fbc3866d39c
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-05-20 03:35:27 +0530

    dispatched gin and processing gin initial push

---
+++ client/src/pages/DashboardPaper/DispatchedGIN.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+//MUI Components
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import IconButton from '@mui/material/IconButton';
+
+//MUI Icons
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+
+//SCSS Style 
+import style from './DispatchedGIN.module.scss';
+
+export default function DispatchedGIN(props) {
+
+    return (
+        <div className={style.container}>
+
+            <div className={style.head}>
+                Dispatched GIN
+            </div>
+
+            <div className={style.body}>
+                <TableContainer>
+                    <Table sx={{ minWidth: 650 }} >
+                        <TableHead sx={{ fontWeight: 900 }}>
+                            <TableRow>
+                                <TableCell sx={{ fontWeight: 700 }}>GIN Number</TableCell>
+                                <TableCell sx={{ fontWeight: 700 }}>Status</TableCell>
+                                <TableCell sx={{ fontWeight: 700 }}>Created By</TableCell>
+                                <TableCell sx={{ fontWeight: 700 }}>Action</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {props.gin.map((row) => (
+                                <TableRow
+                                    key={row.name}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell sx={{ padding: "8px" }}>{row.ginnumber}</TableCell>
+                                    <TableCell sx={{ color: "#FF8400", fontWeight: 700, padding: "8px" }}>{row.status}</TableCell>
+                                    <TableCell component="th" scope="row" sx={{ padding: "8px" }}>{row.createdby} </TableCell>
+                                    <TableCell sx={{ padding: "8px" }}>
+                                        <IconButton>
+                                            <Link to="/delivery-representative/manage-gin">
+                                                <NavigateNextIcon
+                                                    className={style.icon}
+                                                />
+                                            </Link>
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+
+        </div>
+    )
+}
