48fd5867b9c9263a823fd8e9f5aeecff2ade1f8e
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-05-20 03:35:05 +0530

    removed visibility icon and added navigation and added colors to status

--- client/src/pages/DashboardPaper/CustomerOrders.js
+++ client/src/pages/DashboardPaper/CustomerOrders.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 //MUI Components
 import Table from '@mui/material/Table';
@@ -10,39 +11,20 @@ import TableRow from '@mui/material/TableRow';
 import IconButton from '@mui/material/IconButton';
 
 //MUI Icons
-import VisibilityIcon from '@mui/icons-material/Visibility';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 //SCSS style
 import style from './CustomerOrders.module.scss';
 
-//Axios
-import axios from 'axios';
-
 export default function CustomerOrders(props) {
 
-    const handleClick = (orderno) => {
-
-        axios
-            .get(`http://localhost:8080/orders/${orderno}`, {
-                headers: {
-                    'authorization': JSON.parse(sessionStorage.getItem("Auth")).accessToken
-                }
-            })
-            .then(res => {
-                localStorage.setItem(orderno, JSON.stringify(res.data.order));
-                window.open(`http://localhost:3000/store-keeper/view-order-details/${orderno}`, "_blank");
-            })
-            .catch(err => {
-                console.log(err);
-            })
-
-    }
+    const { employeedetails } = props;
 
     return (
         <div className={style.container}>
 
             <div className={style.head}>
-                Processing Invoices
+                Pending Invoices
             </div>
 
             <div className={style.body}>
@@ -64,13 +46,26 @@ export default function CustomerOrders(props) {
                                 >
                                     <TableCell component="th" scope="row" sx={{ padding: "8px" }}>{row.orderno} </TableCell>
                                     <TableCell sx={{ padding: "8px" }}>{row.route}</TableCell>
-                                    <TableCell sx={{ color: "#2196F3", fontWeight: 700, padding: "8px" }}>{row.status}</TableCell>
+                                    <TableCell sx={{ color: "#2196F3", fontWeight: 700, padding: "8px" }}>
+                                        {
+                                            row.status === 'Pending' ?
+                                                <p style={{ padding: "0", margin: "0", color: "#5E01AE", fontWeight: "700" }}>{row.status}</p>
+                                                : row.status === 'Processing' ?
+                                                    <p style={{ padding: "0", margin: "0", color: "#2196F3", fontWeight: "700" }}>{row.status}</p>
+                                                    : row.status === 'Dispatched' ?
+                                                        <p style={{ padding: "0", margin: "0", color: "#eed202", fontWeight: "700" }}>{row.status}</p>
+                                                        : row.status === 'Delivered' ?
+                                                            <p style={{ padding: "0", margin: "0", color: "#FF8400", fontWeight: "700" }}>{row.status}</p>
+                                                            : <p style={{ padding: "0", margin: "0", color: "#4caf50", fontWeight: "700" }}>{row.status}</p>
+                                        }
+                                    </TableCell>
                                     <TableCell sx={{ padding: "8px" }}>
                                         <IconButton>
-                                            <VisibilityIcon
-                                                className={style.icon}
-                                                onClick={() => handleClick(row.orderno)}
-                                            />
+                                            <Link to={`/${employeedetails.designation.replace(/\s+/g, '-').toLowerCase()}/sales-and-invoice`} >
+                                                <NavigateNextIcon
+                                                    className={style.icon}
+                                                />
+                                            </Link>
                                         </IconButton>
                                     </TableCell>
                                 </TableRow>
@@ -80,6 +75,6 @@ export default function CustomerOrders(props) {
                 </TableContainer>
             </div>
 
-        </div>
+        </div >
     )
 }
