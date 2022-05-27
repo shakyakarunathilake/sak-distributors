2d727e52c7b517338d308620a6674eede6ba4e0d
Shakya Karunathilake <46352201+stinger1999@users.noreply.github.com>
2022-05-27 14:57:11 +0530

    removed employee details from manage gin useeffect

--- client/src/pages/StoreKeeper/ManageGIN.js
+++ client/src/pages/StoreKeeper/ManageGIN.js
@@ -104,7 +104,7 @@ export default function ManageGIN() {
                     console.log(error)
                 })
         }
-    }, [reRender, employeedetails]);
+    }, [reRender]);
 
     const getInChargeVehicleOptions = () => {
         axios
